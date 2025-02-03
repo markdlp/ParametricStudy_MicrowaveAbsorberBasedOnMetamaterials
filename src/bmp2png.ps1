180bb3b7f6de4d79cd986b2e434a98963bae6751
Mark Delaportas <delaportas.markos@gmail.com>
2025-02-03 03:01:53 +0200

    adding correct data and embedding videos

--- src/bmp2png.ps1
+++
@@ -1,8 +0,0 @@
-[Reflection.Assembly]::LoadWithPartialName("System.Windows.Forms") | Out-Null
-
-Get-ChildItem -File "img\*.bmp" -Recurse | ForEach-Object {
-    $bitmap = [System.Drawing.Bitmap]::new($_.FullName)
-    $newname = $_.FullName -replace '.bmp$','.png'
-    $bitmap.Save($newname, "png")
-    $bitmap.Dispose()
-}
